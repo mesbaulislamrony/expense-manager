66298e4f1b554339d570ff194987760fe86047f0
mesbaulislamrony <mesbaul.cse26@gmail.com>
2023-03-23 21:50:20 +0600

    add fillable and forgein key

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Expense extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'expense_category_id', 'amount'];
 }
--- database/migrations/2023_03_23_152656_create_expenses_table.php
+++ database/migrations/2023_03_23_152656_create_expenses_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
             $table->integer('expense_category_id');
             $table->double('amount', 10, 2);
             $table->text('note')->nullable();
